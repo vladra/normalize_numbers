71c11301fa7960e626dacefea8b2ffc0670419b2
Vladimir Dralo <vladimir@dralo.me>
2015-10-05 18:57:55 +0300

    Work not only with ActiveRecord attributes, but also with attr_accessor defined attributes

--- lib/normalize_numbers/base.rb
+++ lib/normalize_numbers/base.rb
@@ -7,8 +7,11 @@ module NormalizeNumbers
     module ClassMethods
       def normalize_numbers(*args)
         args.each do |attr|
-          define_method("#{attr}=") do |arg|
-            return super arg.tr(',', '.') if arg.is_a? String
+          method_name = "#{attr}="
+          define_method(method_name) do |arg|
+            new_arg = arg.tr(',', '.')
+            return instance_variable_set("@#{attr}", new_arg) unless defined?(super)
+            return super new_arg if arg.is_a? String
             super
           end
         end
